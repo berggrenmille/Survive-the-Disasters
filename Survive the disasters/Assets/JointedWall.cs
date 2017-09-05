8b3bbfa0277d46a7a3cd55fb52c4a81921e64d78
iceklue <iceklue@gmail.com>
2017-09-05 21:51:36 +0200

    Remade some of the code, all is fixed.
    WRECKING BALL DESTROYED RIP

--- Survive the disasters/Assets/JointedWall.cs
+++ Survive the disasters/Assets/JointedWall.cs
@@ -3,31 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JointedWall : MonoBehaviour {
-    List<Transform> children = new List<Transform>();
-
     public float breakForce = 50f;
     public float breakTourque = 50f;
-	// use this for initialization
-	void Start () {
+    public bool useBreakForceOffset = true;
+    public bool useBreakTourqueOffset = true;
+    public float breakForceOffset = 20f;
+    public float breakTourqueOffset = 20f;
+        // use this for initialization
+        private void Start () {
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform t = transform.GetChild(i);
             RaycastHit hit;
-            //Check for nearby connected objects
+            //Connect to nearby objects
             if (Physics.Raycast(t.position, t.up, out hit, t.GetComponent<Collider>().bounds.extents.y+0.1f))
             {
-                if (hit.collider.CompareTag("Physics Object"))
+                
+                if (hit.collider.gameObject.FindComponentInChildWithTag<Transform>("Jointed"))
                 {
                     FixedJoint joint = t.gameObject.AddComponent<FixedJoint>();
                     joint.connectedBody = hit.rigidbody;
                     joint.breakForce = breakForce;
                     joint.breakTorque = breakTourque;
+                    if (useBreakForceOffset)
+                        joint.breakForce += Random.Range(-breakForceOffset, breakForceOffset);
+                    if (useBreakTourqueOffset)
+                        joint.breakTorque += Random.Range(-breakTourqueOffset, breakTourqueOffset);
                     joint.enableCollision = true;
                 }
             }
             if (Physics.Raycast(t.position, -t.up, out hit, t.GetComponent<Collider>().bounds.extents.y + 0.1f))
             {
-                if (hit.collider.CompareTag("Physics Object"))
+                if (hit.collider.gameObject.FindComponentInChildWithTag<Transform>("Jointed"))
                 {
                     FixedJoint joint = t.gameObject.AddComponent<FixedJoint>();
                     joint.connectedBody = hit.rigidbody;
@@ -38,23 +45,62 @@ public class JointedWall : MonoBehaviour {
             }
             if (Physics.Raycast(t.position, t.right, out hit, t.GetComponent<Collider>().bounds.extents.x + 0.1f))
             {
-                if (hit.collider.CompareTag("Physics Object"))
+                if (hit.collider.gameObject.FindComponentInChildWithTag<Transform>("Jointed"))
                 {
                     FixedJoint joint = t.gameObject.AddComponent<FixedJoint>();
                     joint.connectedBody = hit.rigidbody;
                     joint.breakForce = breakForce;
                     joint.breakTorque = breakTourque;
+                    if (useBreakForceOffset)
+                        joint.breakForce += Random.Range(-breakForceOffset, breakForceOffset);
+                    if (useBreakTourqueOffset)
+                        joint.breakTorque += Random.Range(-breakTourqueOffset, breakTourqueOffset);
                     joint.enableCollision = true;
                 }
             }
             if (Physics.Raycast(t.position, -t.right, out hit, t.GetComponent<Collider>().bounds.extents.x + 0.1f))
             {
-                if (hit.collider.CompareTag("Physics Object"))
+                if (hit.collider.gameObject.FindComponentInChildWithTag<Transform>("Jointed"))
                 {
                     FixedJoint joint = t.gameObject.AddComponent<FixedJoint>();
                     joint.connectedBody = hit.rigidbody;
                     joint.breakForce = breakForce;
                     joint.breakTorque = breakTourque;
+                    if (useBreakForceOffset)
+                        joint.breakForce += Random.Range(-breakForceOffset, breakForceOffset);
+                    if (useBreakTourqueOffset)
+                        joint.breakTorque += Random.Range(-breakTourqueOffset, breakTourqueOffset);
+                    joint.enableCollision = true;
+                }
+            }
+            if (Physics.Raycast(t.position, t.forward, out hit, t.GetComponent<Collider>().bounds.extents.x + 0.1f))
+            {
+                if (hit.collider.gameObject.FindComponentInChildWithTag<Transform>("Jointed"))
+                {
+                    FixedJoint joint = t.gameObject.AddComponent<FixedJoint>();
+                    joint.connectedBody = hit.rigidbody;
+                    joint.breakForce = breakForce;
+                    joint.breakTorque = breakTourque;
+                    if (useBreakForceOffset)
+                        joint.breakForce += Random.Range(-breakForceOffset, breakForceOffset);
+                    if (useBreakTourqueOffset)
+                        joint.breakTorque += Random.Range(-breakTourqueOffset, breakTourqueOffset);
+                    joint.enableCollision = true;
+                }
+            }
+            if (Physics.Raycast(t.position, -t.forward, out hit, t.GetComponent<Collider>().bounds.extents.x + 0.1f))
+            {
+                if (hit.collider.gameObject.FindComponentInChildWithTag<Transform>("Jointed"))
+
+                {
+                    FixedJoint joint = t.gameObject.AddComponent<FixedJoint>();
+                    joint.connectedBody = hit.rigidbody;
+                    joint.breakForce = breakForce;
+                    joint.breakTorque = breakTourque;
+                    if (useBreakForceOffset)
+                        joint.breakForce += Random.Range(-breakForceOffset, breakForceOffset);
+                    if (useBreakTourqueOffset)
+                        joint.breakTorque += Random.Range(-breakTourqueOffset, breakTourqueOffset);
                     joint.enableCollision = true;
                 }
             }
